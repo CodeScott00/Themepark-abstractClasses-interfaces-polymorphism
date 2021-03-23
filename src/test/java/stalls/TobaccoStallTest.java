007d24134d481d6b7eb59108bb58170f54c63f96
CodeScott00 <dougal_00@hotmail.com>
2021-03-23 17:01:43 +0000

    Themepark Abstraction classes, interfaces and polymorphism, MVP Done.

---
+++ src/test/java/stalls/TobaccoStallTest.java
@@ -0,0 +1,39 @@
+package stalls;
+
+import org.junit.Before;
+import org.junit.Test;
+import people.Visitor;
+
+import static org.junit.Assert.assertEquals;
+
+public class TobaccoStallTest {
+
+    TobaccoStall tobaccoStall;
+    Visitor visitor;
+
+    @Before
+    public void setUp() throws Exception {
+        tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1, 1);
+        visitor = new Visitor(19, 170.00, 50.00);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Jacks Drum", tobaccoStall.getName());
+    }
+
+    @Test
+    public void hasOwner() {
+        assertEquals("Jack Jarvis", tobaccoStall.getOwnerName());
+    }
+
+    @Test
+    public void hasParkingSpot() {
+        assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void isAllowedTo(){
+        assertEquals(true, tobaccoStall.isAllowedTo(visitor));
+    }
+}
